b31ca6b0a8c5de4bc56d9cc2af158d08a7e1ed98
Pedro Gustavo Junior <142240229+PedroGustavo99@users.noreply.github.com>
2024-03-10 21:45:34 -0300

    Add files via upload
    Subindo arquivo atualizado

---
+++ sistema_petshop2024/incluir_pessoa_frontend.php
@@ -0,0 +1,18 @@
+<!--Inicio Incluir-->
+<center>
+
+    <body bgcolor="mediumturquoise">
+        <form method="POST" action="incluir_cliente_backend.php">
+            <p><br>
+
+                ID: <input type="text" name="idpessoa"><br><br>
+
+                </select><br><br>
+                Nome: <input type="text" name="nome"><br><br>
+                Idade: <input type="int" name="idade"><br><br>
+                telefone: <input type="text" name="telefone"><br><br>
+
+                <input type="submit" name="Submit" value="Incluir">
+        </form>
+</center>
+<!--Fim Incluir-->
